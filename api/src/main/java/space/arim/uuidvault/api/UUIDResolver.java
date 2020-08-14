643985bcad0f693ee34323c68cf01456f3fc6493
A248 <anandebeh@gmail.com>
2020-08-14 18:04:41 -0400

    Update license headers in all files + Move UUIDUtil

--- api/src/main/java/space/arim/uuidvault/api/UUIDResolver.java
+++ api/src/main/java/space/arim/uuidvault/api/UUIDResolver.java
@@ -1,20 +1,20 @@
 /* 
- * ArimAPI-util
+ * UUIDVault-api
  * Copyright © 2020 Anand Beh <https://www.arim.space>
  * 
- * ArimAPI-util is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * UUIDVault-api is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * ArimAPI-util is distributed in the hope that it will be useful,
+ * UUIDVault-api is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with ArimAPI-util. If not, see <https://www.gnu.org/licenses/>
- * and navigate to version 3 of the GNU General Public License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UUIDVault-api. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
  */
 package space.arim.uuidvault.api;
 
